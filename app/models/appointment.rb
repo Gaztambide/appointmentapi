e3bd54a309c598efab31fb078f4b0b07feec61cf
Gaztambide <francogaztambide@gmail.com>
2016-03-16 01:17:05 -0400

    added validations to appointment model

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,2 +1,6 @@
 class Appointment < ActiveRecord::Base
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :start_time, presence: true, uniqueness: true
+  validates :end_time, presence: true, uniqueness: true
 end
